355259435560f1f6ff5730d2852befb69e2b68fa
gus3inov <gusejnov1999@list.ru>
2018-03-23 21:20:38 +0300

    rename main.js to app.js

---
+++ src/js/app.js
@@ -0,0 +1,93 @@
+import HScrollView from './views/HScrollView';
+import HScrollModel from './models/HScrollModel';
+import HScrollController from './controllers/HScrollController';
+import { fadeIn, fadeOut } from './helpers/BaseDom';
+import initBgSlider from './components/bg-slider';
+import Modal from './helpers/Modal';
+
+/**
+ * @feature About us modal
+ */
+
+const   modalAboutWrap      = document.querySelector('.modal-about'),
+        openModalAboutBtn   = document.querySelector('.modal-about_open'),
+        closeModalAboutBtn  = document.querySelector('.modal-about_close');
+
+const modalAbout = new Modal({
+    modal: modalAboutWrap,
+    openBtn: openModalAboutBtn,
+    closeBtn: closeModalAboutBtn,
+    duration: 200
+})
+
+/**
+ * @feature Horizontal scroll -> categories.html
+ */
+
+let listScroll_1 = document.querySelector('.categories-scroll_1'),
+    listScroll_2 = document.querySelector('.categories-scroll_2');
+
+if (listScroll_1 !== null && listScroll_2 !== null) {
+
+    const scrollModel = new HScrollModel({
+            slideWrapper: listScroll_1,
+            itemsScroll: '.categories-scroll_1 .categories-list__item',
+            scrollBody: '.categories'
+        }),
+        scrollView = new HScrollView(scrollModel),
+        scrollController = new HScrollController(scrollView, scrollModel);
+
+    scrollController.scrollItems();
+}
+
+/**
+ * @feature Categories toggle -> categories.html
+ */
+
+const showCategory = document.querySelector('.btn.open_categories'),
+    categoryItems = document.querySelectorAll('.categories-list .categories-list__item');
+
+if(showCategory !== null && categoryItems !== null){
+    showCategory.addEventListener('click', function () {
+        this.classList.toggle('.back');
+
+        for(let i = 0; i < categoryItems.length; i++){
+            categoryItems[i].classList.toggle('visible');
+            setTimeout(function () {
+                categoryItems[i].classList.toggle('visually_visible');
+            }, 20 * i)
+        }
+    });
+}
+
+/**
+ * @feature Toggle sidebar -> index.html
+ */
+
+let sidebarMenu = document.querySelector('.sidebar-menu'),
+    sidebarBtn = document.querySelector('button[data-open="sidebar"]');
+
+if (sidebarBtn !== null && sidebarMenu !== null) {
+
+    sidebarBtn.addEventListener('click', function () {
+        if (sidebarMenu.classList.contains('visible')) {
+            sidebarMenu.classList.remove('visible');
+            fadeOut(sidebarMenu, 300);
+        } else {
+            sidebarMenu.classList.add('visible');
+            fadeIn(sidebarMenu, 300);
+        }
+    });
+}
+
+/**
+ * @feature Init bg-slider -> bg-slider.html
+ */
+
+const bg_slider = $('.bg-slider');
+
+if(bg_slider.length !== 0){
+    initBgSlider(bg_slider);
+}
+
+
